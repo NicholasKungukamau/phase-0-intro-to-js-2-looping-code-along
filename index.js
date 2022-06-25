42da9a45eab505d71a84289214f9b44900a95428
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-25 10:38:07 +0300

    commit tree 499aab99a227fcb3876a0950cd7c26b29d816d51

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let namesArray = ['John', 'Mary', 'Jane' ]
+
+function writeCards( namesArray, surprise) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!` )
+    }
+    return thankYouCards
+  }
+  writeCards (namesArray, "surprise");
+
+
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
